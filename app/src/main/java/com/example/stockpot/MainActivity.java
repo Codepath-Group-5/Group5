f8c27f38d03125494a565037fcbdece9cd2fc445
mordartri <trentjbuckholz@gmail.com>
2021-03-02 20:22:10 -0800

    Bottom Navigation View with initial fragments. User login implemented.

--- app/src/main/java/com/example/stockpot/MainActivity.java
+++ app/src/main/java/com/example/stockpot/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.stockpot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.stockpot.fragments.HomeFragment;
+import com.example.stockpot.fragments.MessagesFragment;
+import com.example.stockpot.fragments.NotificationsFragment;
+import com.example.stockpot.fragments.ProfileFragment;
+import com.example.stockpot.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.actionHome:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.actionSearch:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.actionMessages:
+                        fragment = new MessagesFragment();
+                        break;
+                    case R.id.actionNotifications:
+                        fragment = new NotificationsFragment();
+                        break;
+                    case R.id.actionProfile:
+                    default:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.actionHome);
     }
 }
--- app/src/main/java/com/example/stockpot/ParseApplication.java
+++ app/src/main/java/com/example/stockpot/ParseApplication.java
@@ -1,4 +1,19 @@
 package com.example.stockpot;
 
-public class ParseApplication {
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("QbGXYqZf40QoZsTFapHmjLerwpflADIXAjNl2R8e")
+                .clientKey("uJt3dTtXAsnladxScHLrHsq9iOr5hUgNJ8Bc7v0q")
+                .server("https://parseapi.back4app.com")
+                        .build()
+                );
+    }
 }
