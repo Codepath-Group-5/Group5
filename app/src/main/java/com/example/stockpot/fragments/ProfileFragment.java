fe1098405d35fb21d7f8f2637785ca562f41a051
manuev2 <manny.4163@gmail.com>
2021-03-09 22:59:48 -0800

    Logout functionality

--- app/src/main/java/com/example/stockpot/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/stockpot/fragments/ProfileFragment.java
@@ -1,17 +1,26 @@
 package com.example.stockpot.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.stockpot.LoginActivity;
 import com.example.stockpot.R;
+import com.parse.ParseUser;
 
 public class ProfileFragment extends Fragment {
-
+    public static final String TAG = "Compose Fragment";
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -22,5 +31,35 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.toolbar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            Log.i(TAG, "id = logout button");
+            logoutUser();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logoutUser() {
+        Log.i(TAG, "Attempting to logout user");
+        ParseUser.logOut();
+        Intent i = new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+        //finish();
     }
 }
